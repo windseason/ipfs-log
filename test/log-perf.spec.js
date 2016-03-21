6976254b21281afffc3aba38de3fda070b64035f
haad <haad@headbanggames.com>
2016-03-21 14:28:43 +0100

    Disable perf tests for CI

--- test/log-perf.spec.js
+++ test/log-perf.spec.js
@@ -1,50 +1,50 @@
-'use strict';
+// 'use strict';
 
-const _      = require('lodash');
-const assert = require('assert');
-const async  = require('asyncawait/async');
-const await  = require('asyncawait/await');
-const ipfsd  = require('ipfsd-ctl');
-const Log    = require('../lib/log');
-const Node   = require('../lib/node');
+// const _      = require('lodash');
+// const assert = require('assert');
+// const async  = require('asyncawait/async');
+// const await  = require('asyncawait/await');
+// const ipfsd  = require('ipfsd-ctl');
+// const Log    = require('../lib/log');
+// const Node   = require('../lib/node');
 
-let ipfs;
+// let ipfs;
 
-const startIpfs = () => {
-  return new Promise((resolve, reject) => {
-    ipfsd.disposableApi((err, ipfs) => resolve(ipfs));
-  });
-};
+// const startIpfs = () => {
+//   return new Promise((resolve, reject) => {
+//     ipfsd.disposableApi((err, ipfs) => resolve(ipfs));
+//   });
+// };
 
-describe('ipfs-log - Performance Measurement', async(function() {
-  this.timeout(60000);
-  before(async((done) => {
-    try {
-      ipfs = await(startIpfs());
-    } catch(e) {
-      console.log(e);
-      assert.equal(e, null);
-    }
-    done();
-  }));
+// describe('ipfs-log - Performance Measurement', async(function() {
+//   this.timeout(60000);
+//   before(async((done) => {
+//     try {
+//       ipfs = await(startIpfs());
+//     } catch(e) {
+//       console.log(e);
+//       assert.equal(e, null);
+//     }
+//     done();
+//   }));
 
-  it.only('add', async((done) => {
-    let ms = 0;
+//   it.only('add', async((done) => {
+//     let ms = 0;
 
-    for(let t = 100; t <= 1000; t += 300) {
-      const log = await(Log.create(ipfs, 'A'));
-      const startTime = new Date().getTime();
+//     for(let t = 100; t <= 1000; t += 300) {
+//       const log = await(Log.create(ipfs, 'A'));
+//       const startTime = new Date().getTime();
 
-      for(let i = 0; i < t; i ++) {
-        await(log.add("hello" + i));
-      }
+//       for(let i = 0; i < t; i ++) {
+//         await(log.add("hello" + i));
+//       }
 
-      const endTime = new Date().getTime();
-      console.log(`  > ${t} took ${(endTime - startTime)} ms`)
-    }
+//       const endTime = new Date().getTime();
+//       console.log(`  > ${t} took ${(endTime - startTime)} ms`)
+//     }
 
-    assert.equal(true, true);
-    done();
-  }));
+//     assert.equal(true, true);
+//     done();
+//   }));
 
-}));
+// }));
