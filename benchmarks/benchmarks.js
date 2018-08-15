0f8504239e1cb41cc088dfcbeb6f67f3b2af9dfb
Kia Rahimian <kia@tintmail.com>
2018-08-15 12:29:34 -0400

    feat: add toMultihash benchmarks

--- benchmarks/benchmarks.js
+++ benchmarks/benchmarks.js
@@ -9,6 +9,7 @@ const heads = require('./heads.js')
 const join = require('./join.js')
 const tailHashes = require('./tail-hashes.js')
 const tails = require('./tails.js')
+const toMultihash = require('./to-multihash.js')
 const toString = require('./to-string.js')
 const values = require('./values.js')
 
@@ -21,6 +22,7 @@ module.exports = [
   ...join,
   ...tailHashes,
   ...tails,
+  ...toMultihash,
   ...toString,
   ...values
 ]
