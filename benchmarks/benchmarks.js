0c1d4579d6b686cd07f0e17c17c12552d5146229
Kia Rahimian <kia@tintmail.com>
2018-08-15 11:33:48 -0400

    feat: add heads benchmarks

--- benchmarks/benchmarks.js
+++ benchmarks/benchmarks.js
@@ -3,12 +3,14 @@ const Log = require('../src/log')
 
 const append = require('./append.js')
 const fromEntryHash = require('./from-entry-hash.js')
+const heads = require('./heads.js')
 const join = require('./join.js')
 const values = require('./values.js')
 
 module.exports = [
   ...append,
   ...fromEntryHash,
+  ...heads,
   ...join,
   ...values
 ]
