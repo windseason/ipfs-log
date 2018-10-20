aa7ec8b3b8028e21ff10a99f78c64dd4f9e0b650
haad <haad@haja.io>
2018-10-20 06:36:46 +0200

    Move log append tests to their own file

---
+++ test/log-append.spec.js
@@ -0,0 +1,133 @@
+'use strict'
+
+const assert = require('assert')
+const rmrf = require('rimraf')
+const Keystore = require('orbit-db-keystore')
+const LogCreator = require('./utils/log-creator')
+const bigLogString = require('./fixtures/big-log.fixture.js')
+const Clock = require('../src/lamport-clock')
+const Entry = require('../src/entry')
+const Log = require('../src/log')
+const { AccessController, IdentityProvider } = Log
+
+// Test utils
+const {
+  config,
+  testAPIs,
+  startIpfs,
+  stopIpfs
+} = require('./utils')
+
+let ipfs, testIdentity, testIdentity2, testIdentity3, testIdentity4
+
+const last = (arr) => {
+  return arr[arr.length - 1]
+}
+
+Object.keys(testAPIs).forEach((IPFS) => {
+  describe('Log - Append (' + IPFS + ')', function () {
+    this.timeout(config.timeout)
+
+    const keystore = Keystore.create(config.testKeysPath)
+    const identitySignerFn = async (id, data) => {
+      const key = await keystore.getKey(id)
+      return keystore.sign(key, data)
+    }
+    const testACL = new AccessController()
+
+    before(async () => {
+      rmrf.sync(config.defaultIpfsConfig.repo)
+      testIdentity = await IdentityProvider.createIdentity(keystore, 'userA', identitySignerFn)
+      testIdentity2 = await IdentityProvider.createIdentity(keystore, 'userB', identitySignerFn)
+      testIdentity3 = await IdentityProvider.createIdentity(keystore, 'userC', identitySignerFn)
+      testIdentity4 = await IdentityProvider.createIdentity(keystore, 'userD', identitySignerFn)
+      ipfs = await startIpfs(IPFS, config.defaultIpfsConfig)
+    })
+
+    after(async () => {
+      await stopIpfs(ipfs)
+      rmrf.sync(config.defaultIpfsConfig.repo)
+    })
+
+    describe('append', () => {
+      describe('append one', async () => {
+        let log
+
+        before(async () => {
+          log = new Log(ipfs, testACL, testIdentity, 'A')
+          await log.append('hello1')
+        })
+
+        it('added the correct amount of items', () => {
+          assert.strictEqual(log.length, 1)
+        })
+
+        it('added the correct values', async () => {
+          log.values.forEach((entry) => {
+            assert.strictEqual(entry.payload, 'hello1')
+          })
+        })
+
+        it('added the correct amount of next pointers', async () => {
+          log.values.forEach((entry) => {
+            assert.strictEqual(entry.next.length, 0)
+          })
+        })
+
+        it('has the correct heads', async () => {
+          log.heads.forEach((head) => {
+            assert.strictEqual(head.hash, log.values[0].hash)
+          })
+        })
+
+        it('updated the clocks correctly', async () => {
+          log.values.forEach((entry) => {
+            assert.strictEqual(entry.clock.id, testIdentity.publicKey)
+            assert.strictEqual(entry.clock.time, 1)
+          })
+        })
+      })
+
+      describe('append 100 items to a log', async () => {
+        const amount = 100
+        const nextPointerAmount = 64
+
+        let log
+
+        before(async () => {
+          log = new Log(ipfs, testACL, testIdentity, 'A')
+          for (let i = 0; i < amount; i++) {
+            await log.append('hello' + i, nextPointerAmount)
+            // Make sure the log has the right heads after each append
+            const values = log.values
+            assert.strictEqual(log.heads.length, 1)
+            assert.strictEqual(log.heads[0].hash, values[values.length - 1].hash)
+          }
+        })
+
+        it('added the correct amount of items', () => {
+          assert.strictEqual(log.length, amount)
+        })
+
+        it('added the correct values', async () => {
+          log.values.forEach((entry, index) => {
+            assert.strictEqual(entry.payload, 'hello' + index)
+          })
+        })
+
+        it('updated the clocks correctly', async () => {
+          log.values.forEach((entry, index) => {
+            assert.strictEqual(entry.clock.time, index + 1)
+            assert.strictEqual(entry.clock.id, testIdentity.publicKey)
+          })
+        })
+
+        it('added the correct amount of next pointers', async () => {
+          log.values.forEach((entry, index) => {
+            assert.strictEqual(entry.next.length, Math.min(index, nextPointerAmount))
+          })
+        })
+      })
+    })
+  })
+})
