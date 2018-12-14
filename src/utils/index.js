f6fdba2425c38583ad47b8fc938dc4cbca668ca0
haad <haad@haja.io>
2018-12-14 11:24:44 +0100

    Remove intersection from utils as obsolete

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,10 @@
 
 const difference = require('./difference')
 const findUniques = require('./find-uniques')
-const intersection = require('./intersection')
 const isDefined = require('./is-defined')
 
 module.exports = {
   difference,
   findUniques,
-  intersection,
   isDefined
 }
