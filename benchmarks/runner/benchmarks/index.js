e871723a4c3b0e0820df1482dd7a8d2c0647a9af
Kia Rahimian <kia@tintmail.com>
2019-01-01 14:26:38 -0500

    feat: add traverse benchmarks

--- benchmarks/runner/benchmarks/index.js
+++ benchmarks/runner/benchmarks/index.js
@@ -10,6 +10,7 @@ const tailHashes = require('./tail-hashes.js')
 const tails = require('./tails.js')
 const toMultihash = require('./to-multihash.js')
 const toString = require('./to-string.js')
+const traverse = require('./traverse.js')
 const values = require('./values.js')
 
 module.exports = [
@@ -25,5 +26,6 @@ module.exports = [
   ...tails,
   ...toMultihash,
   ...toString,
+  ...traverse,
   ...values
 ]
