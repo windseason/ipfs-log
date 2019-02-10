d6228361f5d733bff86372f0db08acbfa6581b4e
Mark Henderson <henderson.mark@gmail.com>
2019-02-10 15:04:00 -0500

    chore: linting

--- test/log-iterator.spec.js
+++ test/log-iterator.spec.js
@@ -2,8 +2,6 @@
 
 const assert = require('assert')
 const rmrf = require('rimraf')
-const Clock = require('../src/lamport-clock')
-const Entry = require('../src/entry')
 const Log = require('../src/log')
 const AccessController = Log.AccessController
 const IdentityProvider = require('orbit-db-identity-provider')
@@ -16,11 +14,7 @@ const {
   stopIpfs
 } = require('./utils')
 
-let ipfs, testIdentity, testIdentity2, testIdentity3, testIdentity4
-
-const last = (arr) => {
-  return arr[arr.length - 1]
-}
+let ipfs, testIdentity
 
 Object.keys(testAPIs).forEach((IPFS) => {
   describe('Log - Iterator (' + IPFS + ')', function () {
@@ -35,9 +29,6 @@ Object.keys(testAPIs).forEach((IPFS) => {
     before(async () => {
       rmrf.sync(ipfsConfig.repo)
       testIdentity = await IdentityProvider.createIdentity({ id: 'userA', identityKeysPath, signingKeysPath })
-      testIdentity2 = await IdentityProvider.createIdentity({ id: 'userB', identityKeysPath, signingKeysPath })
-      testIdentity3 = await IdentityProvider.createIdentity({ id: 'userC', identityKeysPath, signingKeysPath })
-      testIdentity4 = await IdentityProvider.createIdentity({ id: 'userD', identityKeysPath, signingKeysPath })
       ipfs = await startIpfs(IPFS, ipfsConfig)
     })
 
@@ -47,13 +38,10 @@ Object.keys(testAPIs).forEach((IPFS) => {
     })
 
     describe('Basic iterator functionality', () => {
-      let log1, log2, log3, log4
+      let log1
 
       beforeEach(async () => {
         log1 = new Log(ipfs, testACL, testIdentity, { logId: 'X' })
-        log2 = new Log(ipfs, testACL, testIdentity2, { logId: 'X' })
-        log3 = new Log(ipfs, testACL, testIdentity3, { logId: 'X' })
-        log4 = new Log(ipfs, testACL, testIdentity4, { logId: 'X' })
 
         for (let i = 0; i <= 100; i++) {
           await log1.append('entry' + i)
